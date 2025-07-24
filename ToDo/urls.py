aeaf1ebe70432be62ddd56318f75ff60699d5025
Glashiii <glashyep@gmail.com>
2025-07-24 19:55:20 +0300

    Add redirect to log in by default

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
-    path('', include('accounts.urls')),
+    path('', RedirectView.as_view(
+        url='/accounts/login/',
+        permanent=False
+    )),
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
 ]
